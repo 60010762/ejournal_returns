9ecab1af3682e2f1df86acf72406356f3967b31e
Sergey Sokolov <73172283+60010762@users.noreply.github.com>
2020-12-01 18:53:15 +0600

    Delete db_config.php

--- sys/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    define('DB_USER', "60010762"); //логин админа БД
-    define('DB_PASSWORD', "AAde4718"); // пароль админа БД
-    define('DB_DATABASE', "LMDEV719"); // база данных
-    define('DB_SERVER', "10.80.176.27"); // сервер 'localhost'
-	define('DB_CONNSTR', "host=".DB_SERVER." port=5432 dbname=".DB_DATABASE." user=".DB_USER." password=".DB_PASSWORD);
-	?>
